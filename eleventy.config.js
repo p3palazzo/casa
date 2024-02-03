546f515f787806ad88d1e0bcc6e7febaa2ac76e3
Pedro P. Palazzo <palazzo@unb.br>
2024-02-03 20:50:04 -0300

    Configurações aprimoradas e alguns filtros

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,7 +3,15 @@
  ****************/
 // First create variables that require() any packages we need
 // const plugin = require('some-eleventy-plugin-package')
-// const sassPlugin = require('eleventy-plugin-sass');
+const Image = require("@11ty/eleventy-img");
+const EleventyFetch = require("@11ty/eleventy-fetch");
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+const path = require('path');
+const Pandoc = require("markdown-it-pandoc");
+const eleventyCiteproc = require("eleventy-plugin-citeproc");
+const htmlmin = require("html-minifier");
 /********************************
  * eleventyConfig function {{{1 *
  ********************************/
@@ -16,36 +24,76 @@ module.exports = function(eleventyConfig) {
   *************************/
   // Copy assets/ to _site/assets
   eleventyConfig.addPassthroughCopy("assets");
+	/*
+	 *eleventyConfig.addPassthroughCopy({ "node_modules/bootstrap/dist/css": "assets/css" });
+	 *eleventyConfig.addPassthroughCopy({ "node_modules/bootstrap/dist/js": "assets/bootstrap/js" });
+	 *eleventyConfig.addPassthroughCopy({ "node_modules/jquery/dist": "assets/jquery/js" });
+	 */
   // emulate passthrough during --serve:
-  //eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+  eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+ /***************
+	* Pandoc {{{2 *
+	***************/
+	eleventyConfig.setLibrary(
+		'md',
+		require('markdown-it')({
+			html: true,
+			linkify: true,
+			typographer: true,
+			katex: false,
+			mathjax: true,
+		}).use(require('markdown-it-pandoc'))
+	);
  /*************************
   * Activate plugins {{{2 *
   *************************/
-  /*
-   *eleventyConfig.addPlugin(sassPlugin, {
-   *  watch: ['assets/css/*.scss'],
-   *  outputDir: '_site/assets/css',
-   *});
-   */
+	// Call filters defined outside this function
+	eleventyConfig.addFilter('dateFilter', dateFilter);
+	eleventyConfig.addFilter('w3DateFilter', w3DateFilter);
+  //eleventyConfig.setQuietMode(true);
+	// https://github.com/Myllaume/eleventy-plugin-citeproc/
+	/*
+	 *eleventyConfig.addPlugin(eleventyCiteproc, {
+	 *  bibliographicStylePath: path.join(__dirname, 'assets/biblio/chicago-note-bibliography.csl'),
+	 *  bibliographicLocalizationPath: path.join(__dirname, 'assets/biblio/locales-pt-BR.xml'),
+	 *  bibliographicDataPath: path.join(__dirname, '_data/biblio.json')
+	 *});
+	 */
+	eleventyConfig.addTransform("htmlmin", function(content) {
+		// Prior to Eleventy 2.0: use this.outputPath instead
+		if( this.page.outputPath && this.page.outputPath.endsWith(".html") ) {
+			let minified = htmlmin.minify(content, {
+				useShortDoctype: true,
+				removeComments: true,
+				collapseWhitespace: true
+			});
+			return minified;
+		}
+		return content;
+	});
  /********************
   * Setup views {{{2 *
   ********************/
-  /*
-   *eleventyConfig.addCollection("obras", function(collection) {
-   *  return collection.getFilteredByGlob("src/obra/*.md");
-   *});
-   */
+  eleventyConfig.addCollection("obras", function(collection) {
+    return sortByDisplayOrder(collection.getFilteredByGlob("src/obra/*.md"));
+  });
+	eleventyConfig.addCollection('destaques', function(collection) {
+    return sortByDisplayOrder(collection.getFilteredByGlob("src/obra/*.md")).filter(
+			x => x.data.featured
+		);
+	});
  /*******************************************************
   * Return is the last instruction to be evaluated {{{2 *
   *******************************************************/
   // If needed, return an object configuration
   return {
-    // Options here
     dir: {
+			htmlTemplateEngine: "liquid",
+			templateFormats: ["html", "liquid", "njk"],
       input: 'src',
       output: '_site',
       includes: '_includes'
     }
   }
-}
+};
 // vim: foldmethod=marker shiftwidth=2 tabstop=2
