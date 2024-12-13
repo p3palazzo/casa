9887ae982619b489dc17e40a363cb45923855770
Pedro P. Palazzo <palazzo@unb.br>
2024-12-12 22:45:03 -0300

    Resolve responsividade da navegação

--- .eleventy.js
+++ .eleventy.js
@@ -32,6 +32,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "node_modules/leaflet/dist": "assets/leaflet" });
 	eleventyConfig.addPassthroughCopy({ "node_modules/jquery/dist": "assets/jquery/js" });
 	eleventyConfig.addPassthroughCopy({ "node_modules/@knight-lab/timelinejs/dist": "assets/timelinejs" });
+  eleventyConfig.addPassthroughCopy({ "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "assets/js/bootstrap.bundle.min.js" });
 	eleventyConfig.addPassthroughCopy("src/.domains");
   eleventyConfig.addPassthroughCopy(".gitattributes");
   // emulate passthrough during --serve:
--- assets/js/main.js
+++ assets/js/main.js
@@ -7,13 +7,13 @@
 // Import just what we need
 
 // import '../../node_modules/bootstrap/js/dist/alert';
-import '../../node_modules/bootstrap/js/dist/button';
+//import '../../node_modules/bootstrap/js/dist/button';
 // import '../../node_modules/bootstrap/js/dist/carousel';
-import '../../node_modules/bootstrap/js/dist/collapse';
+//import '../../node_modules/bootstrap/js/dist/collapse';
 // import '../../node_modules/bootstrap/js/dist/dropdown';
-import '../../node_modules/bootstrap/js/dist/modal';
+//import '../../node_modules/bootstrap/js/dist/modal';
 // import '../../node_modules/bootstrap/js/dist/offcanvas';
-import '../../node_modules/bootstrap/js/dist/popover';
+//import '../../node_modules/bootstrap/js/dist/popover';
 // import '../../node_modules/bootstrap/js/dist/scrollspy';
 // import '../../node_modules/bootstrap/js/dist/tab';
 // import '../../node_modules/bootstrap/js/dist/toast';
@@ -24,8 +24,6 @@ import '../../node_modules/bootstrap/js/dist/popover';
 //
 // Place any custom JS here
 //
-
-import "../../_js/search.js";
 
 // Create an example popover
 document.querySelectorAll('[data-bs-toggle="popover"]')
